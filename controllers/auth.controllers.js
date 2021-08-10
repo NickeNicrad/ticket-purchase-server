c57b02683f20a7d0ce76b46b30ad42975f556979
Nicke Nicrad <nickenicrad@gmail.com>
2021-08-10 16:50:55 +0200

    bcrypt issue fixed

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Admin = require('../models/admin.model.js');
 const User = require('../models/user.model.js');
